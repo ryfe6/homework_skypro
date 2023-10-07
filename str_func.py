79080e68677e6b0df97ad7378a02944ad428664e
ryfe6 <135255451+ryfe6@users.noreply.github.com>
2023-10-07 18:47:46 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -3,5 +3,6 @@ def сptlz(word_line):
     return word_line.capitalize()
 
 
-    """главными буквами."""
-    return word_line.upper()
+def сptlz(word_line):
+    """Функция принимает слово и возвращает его с заглавной буквой в начале."""
+    return word_line.capitalize()
