db44a90de35df2a232252fe846764d36d923b048
ryfe <denis.koptelev@gmail.com>
2023-10-07 18:20:59 +0300

    Добавлены докстринги и функция cptlz

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def up(word_line):
+    """Функция принимает слово и возвращает его со веми заглавными буквами."""
     return word_line.upper()
 
+
+def сptlz(word_line):
+    """Функция принимает слово и возвращает его с заглавной буквой в начале."""
+    return word_line.capitalize()
